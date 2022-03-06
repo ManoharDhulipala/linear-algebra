5e0d26c905b66b037cbeacaced8e073993be12cd
Ethan Coe-Renner <e.coerenner@gmail.com>
2022-03-05 18:31:38 -0800

    determinant function works

--- matrix.py
+++ matrix.py
@@ -1,7 +1,7 @@
 class Matrix:
     def __init__(self, size): ## creates empty matrix of given size
         self.size = size
-        self.matrix = [[0]*size]*size # creates empty 2d array of appropriate size
+        self.matrix = [[0 for i in range(size)] for i in range(size)] # creates empty 2d array of appropriate size
 
     def new(matrix): # expects a 2d array as input, creates a matrix object
         size = len(matrix)
@@ -15,45 +15,46 @@ class Matrix:
             print("  ", row)
         print("]")
 
+    def copy_matrix(self):
+        size = self.size
 
-    # returns the matrix when the minors at x, y are removed
-    def minors(self, row, col):
-        minors = Matrix(self.size - 1)
+        newmatrix = Matrix(size)
 
-        for (i,v) in enumerate(self.matrix):
-            for (j, value) in enumerate(v):
-                if i != row and j != col:
-                    print(i - (i > row),j - (j > col), ": ", value)
-                    minors.matrix[i - (i > row)][j - (j > col)] = value
-                    # TODO: modifies more than one row at a time
-                    minors.print()
-        minors.print()
-        return minors
+        for i in range(size):
+            for j in range(size):
+                newmatrix.matrix[i][j] = self.matrix[i][j]
 
-        
-    def det(self):
-        print(self.size, len(self.matrix), len(self.matrix[0]))
-        if self.size < 2:
-            return self.matrix[0][0]
-        elif self.size == 2:
-            return self.matrix[0][0] * self.matrix[1][1] - self.matrix[0][1] * self.matrix[1][0]
-        else:
-            sum = 0
-            for (i,v) in enumerate(self.matrix[0]): # loop through first row
-                sum += ((-1) ** i) * self.minors(0,i).det()
-            return sum
+        return newmatrix
+
+    def det(self, total=0):
+        indices = list(range(len(self.matrix)))
+
+        # base case with 2x2
+        if self.size == 2:
+            val = self.matrix[0][0] * self.matrix[1][1] - self.matrix[1][0] * self.matrix[0][1]
+            return val
+
+        # get rid of minors for each item in first row, get matrix and add to total
+        for focus in indices:  # for each focus column, find the submatrix ...
+            As = self.copy_matrix()  # make a copy, and ...
+            As.matrix = As.matrix[1:]  # ... remove the first row
+            As.size -= 1
+            height = len(As.matrix)
+
+            for i in range(height):  
+                As.matrix[i] = As.matrix[i][0:focus] + As.matrix[i][focus+1:]  
+
+            sign = (-1) ** (focus % 2)  
+            sub_det = As.det()  # recursively call det
+            total += sign * self.matrix[0][focus] * sub_det  
+        return total
+
                 
-m2 = Matrix.new([[0,1,4,32],[3,1,4,40],[6,0,2,32],[3,1,6,60]])
 
-m2.print()
-
-m2.minors(1,1).print()
-
-# print(m2.det())
-
-# twoxtwo = Matrix.new([[1,2],[3,4]])
-# twoxtwo.print()
-# print(twoxtwo.minors(0,0).det())
+m2 = [[1,1,4],[3,1,4],[6,2,2]]
+m = Matrix.new(m2)
+m.print()
+print(m.det())
 
 
 
