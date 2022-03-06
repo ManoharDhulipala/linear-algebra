60935bce5922ca356862e83204580bd063d54eb5
ManoharDhulipala <98207071+ManoharDhulipala@users.noreply.github.com>
2022-03-06 12:58:06 -0800

    Included the addmatrix function

--- matrix.py
+++ matrix.py
@@ -48,13 +48,28 @@ class Matrix:
             sub_det = As.det()  # recursively call det
             total += sign * self.matrix[0][focus] * sub_det  
         return total
+    def add(self, other, other1):
+
+      for i in range(len(self.matrix)):  
+        for j in range(len(self.matrix[0])):
+
+          self.matrix[i][j] = other1.matrix[i][j] + other.matrix[i][j]
+
+      return self
 
                 
-
+m1 = [[0,0,0],[0,0,0],[0,0,0]]
 m2 = [[1,1,4],[3,1,4],[6,2,2]]
+m3 = [[2,2,5],[4,2,5],[7,3,3]]
 m = Matrix.new(m2)
+n = Matrix.new(m3)
+o = Matrix.new(m1)
 m.print()
 print(m.det())
+
+o = o.add(m,n)
+o.print()
+
 
 
 
